9d021066ae132f53e7a6506fd741c94ed4939ea0
Diegoav87 <diegoabdov@gmail.com>
2021-12-20 09:30:28 -0600

    S3 now working

--- devcamper/settings.py
+++ devcamper/settings.py
@@ -208,6 +208,7 @@ if USE_S3:
     AWS_LOCATION = 'static'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'devcamper.storages.MediaStore'
 else:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
