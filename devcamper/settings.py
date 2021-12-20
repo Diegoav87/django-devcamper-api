c2ef3423319fa170f3e28ed0c58822b64b050907
Diegoav87 <diegoabdov@gmail.com>
2021-12-19 21:41:50 -0600

    Still battling

--- devcamper/settings.py
+++ devcamper/settings.py
@@ -32,8 +32,10 @@ DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'devcamper-django.herokuapp.com']
 
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get(
+    'ENV') == 'HEROKU' else os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get(
+    'ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
 
 # if os.name == 'nt':
 #     import platform
